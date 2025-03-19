682cb610ddd2e78032e76a7e3dc59fc44d2ea02e
Sonny-in-1999 <minsoo1022@gmail.com>
2025-03-19 15:26:09 +0900

    feat: project init

---
+++ src/main/java/com/example/springtodosubject/SpringTodoSubjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtodosubject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoSubjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTodoSubjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springtodosubject/SpringTodoSubjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtodosubject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoSubjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
