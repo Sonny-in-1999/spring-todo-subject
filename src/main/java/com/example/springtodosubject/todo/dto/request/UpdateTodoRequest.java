ba5e6bcc511efbf8f7e43208e74fc39903af2c1e
Sonny-in-1999 <minsoo1022@gmail.com>
2025-04-02 17:49:20 +0900

    fix: UpdateTodoRequest 일정의 기간 필드값 제약 조건 추가

--- src/main/java/com/example/springtodosubject/todo/dto/request/UpdateTodoRequest.java
+++ src/main/java/com/example/springtodosubject/todo/dto/request/UpdateTodoRequest.java
@@ -1,8 +1,6 @@
 package com.example.springtodosubject.todo.dto.request;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public record UpdateTodoRequest(
         @Size(max = 200, message = "일정은 200자 이내로 작성되어야 합니다.")
@@ -14,10 +12,12 @@ public record UpdateTodoRequest(
 
         @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "날짜 형식은 yyyy-MM-dd 이어야 합니다.")
         @NotNull(message = "일정 시작일자가 반드시 포함되어야 합니다.")
+        @Past(message = "과거 날짜만 입력 가능합니다.")
         String startDate,
 
         @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "날짜 형식은 yyyy-MM-dd 이어야 합니다.")
         @NotNull(message = "일정 종료일자가 반드시 포함되어야 합니다.")
+        @Future(message = "미래 날짜만 입력 가능합니다.")
         String endDate
 ) {
 }
